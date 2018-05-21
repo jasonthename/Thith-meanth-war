b3dd4b98f5361c163217426e54beeffa8a4fcad4
JasonJay <jjackson8895@gmail.com>
2018-05-21 14:04:32 -0400

    First commit

---
+++ main.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+print "Enter a string: "
+user_input = gets.chomp
+user_input.downcase!
+
+if user_input.include? "s"
+  user_input.gsub!(/s/, "th")
+else print "Nothing here to do..."
+end
+
+puts "Bye, #{user_input}!"
